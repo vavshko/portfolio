2cbc9643b5ae709d51ba9aee8fac94e2c04a35f3
AH\User <hivens.mailbox@gmail.com>
2020-11-13 16:08:14 +0200

    structure update

--- backend/catalog/index.php
+++ backend/catalog/index.php
@@ -7,9 +7,10 @@ $categories = get_cat();
 $categories_tree = map_tree($categories);
 $categories_menu = categories_to_string($categories_tree);
 ?>
-
+<?php phpinfo();?>
 <!DOCTYPE html>
 <html lang="en">
+
 
 <head>
     <meta charset="UTF-8">
